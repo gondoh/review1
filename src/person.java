598c3daa8aea38cdbd2bd86f89cd80ba6387de12
gondoh <gondoh@gufii.net>
2012-08-09 20:17:00 +0900

    test up.

---
+++ src/person.java
@@ -0,0 +1,43 @@
+
+class Person {
+
+	public String uji = "氏";
+	public String na = "名";
+
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void showName() {
+		System.out.plintln(this.uji + "：" + this.lastName + " " + this.na + "：" + this.firstName);
+	}
+
+	public static void main(String[] args) {
+		Person person1 = new Person();
+		person1.setLastName("和田");
+		person1.setFirstName("充史");
+		person1.showName();
+
+		Person person2 = new Person();
+		person2.setLastName("和田");
+		person2.setFirstName("充史");
+		person2.showName();
+		
+	}
+
+}
